f43a61009863ca6838bf9b45fe9cfb2150f480cf
Jen Diamond <thejendiamond@gmail.com>
2014-05-28 15:53:57 -0700

    adds a procfile

--- db/migrate/20140423201104_create_posts.rb
+++ db/migrate/20140423201104_create_posts.rb
@@ -5,7 +5,7 @@ class CreatePosts < ActiveRecord::Migration
       t.text :body
       t.timestamps
     end
-    Post.create(title: "Hosting Sinatra on Heroku", body: "Just use Posgres.")
+    Post.create(title: "My first post", body: "And this is the post's content.")
     Post.create(title: "How to lasso your dog", 
                 body: "1. Tie a rope into a lasso. 2. Swing it over that unruly dog's torso. 3. Gently pull.")
     Post.create(title: "Top 10 coffee shops in Austin", body: "1..10: Epoch Coffee, the 24/7 coffee shop.")
