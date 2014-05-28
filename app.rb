021e1a58f587dd7ea5024e98ec177df7ab8b609e
Jen Diamond <thejendiamond@gmail.com>
2014-05-28 01:19:13 -0700

    changes page names

--- app.rb
+++ app.rb
@@ -1,28 +1,28 @@
 # app.rb
 require "sinatra"
 require "sinatra/activerecord"
- 
+
 set :database, "sqlite3:///blog.db"
- 
+
 class Post < ActiveRecord::Base
 end
 
 helpers do
-  # If @title is assigned, add it to the page's title.
+  # If @title is assigned, add it to the page's title
   def title
     if @title
-      "#{@title} -- My Blog"
+      "#{@title} -- The Standard Librarians Blog"
     else
       "My Blog"
     end
   end
- 
+
   # Format the Ruby Time object returned from a post's created_at method
-  # into a string that looks like this: 06 Jan 2012
+  # into a string that looks like this: July 01 2014
   def pretty_date(time)
-   time.strftime("%d %b %Y")
+   time.strftime("%b %d %Y")
   end
- 
+
 end
 
 get "/" do
@@ -35,19 +35,19 @@ get "/" do
   @posts = Post.order("created_at DESC")
   erb :"posts/index"
 end
- 
+
 # Get the New Post form
 get "/posts/new" do
   @title = "New Post"
   @post = Post.new
   erb :"posts/new"
 end
- 
+
 # The New Post form sends a POST request (storing data) here
 # where we try to create the post it sent in its params hash.
 # If successful, redirect to that post. Otherwise, render the "posts/new"
-# template where the @post object will have the incomplete data that the 
-# user can modify and resubmit.
+# template where the @post object will have the incomplete data that the
+# user can modify and re-submit.
 post "/posts" do
   @post = Post.new(params[:post])
   if @post.save
@@ -56,23 +56,23 @@ post "/posts" do
     erb :"posts/new"
   end
 end
- 
-# Get the individual page of the post with this ID.
+
+# Get the individual page of the post with this ID
 get "/posts/:id" do
   @post = Post.find(params[:id])
   @title = @post.title
   erb :"/posts/show"
 end
- 
-# Get the Edit Post form of the post with this ID.
+
+# Get the Edit Post form of the post with this ID
 get "/posts/:id/edit" do
   @post = Post.find(params[:id])
   @title = "Edit Form"
   erb :"posts/edit"
 end
- 
+
 # The Edit Post form sends a PUT request (modifying data) here.
-# If the post is updated successfully, redirect to it. Otherwise,
+# If the post is updated successfully, redirect to it; otherwise,
 # render the edit form again with the failed @post object still in memory
 # so they can retry.
 put "/posts/:id" do
@@ -83,15 +83,21 @@ put "/posts/:id" do
     erb :"posts/edit"
   end
 end
- 
-# Deletes the post with this ID and redirects to homepage.
+
+# Deletes the post with this ID and redirects to homepage
 delete "/posts/:id" do
   @post = Post.find(params[:id]).destroy
   redirect "/"
 end
- 
-# Our About Me page.
-get "/about" do
-  @title = "About Me"
-  erb :"pages/about"
+
+# Our Contributors page
+get "/contributors" do
+  @title = "Contributors"
+  erb :"pages/contributors"
+end
+
+# References page
+get "/references" do
+  @title = "References"
+  erb :"pages/references"
 end
