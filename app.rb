76ea097b999e06a43b2a6f8f01fce381f06f9cab
Jen Diamond <thejendiamond@gmail.com>
2014-07-08 10:31:01 -0700

    adds new page

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 module Nesta
   class App
-  	def author_biography(name = nil)
+    def author_biography(name = nil)
       name ||= @page.metadata('author')
       if name
         template = name.downcase.gsub(/\W+/, '_').to_sym
@@ -52,6 +52,37 @@ EOF
       @body_class = 'category ' + @body_class if @page.is_category?
       set_from_page(:description, :keywords)
       cache haml(@page.template, :layout => @page.layout)
+    end
+
+    helpers do
+      def list_articles(articles)
+        haml_tag :ol do
+          articles.each do |article|
+            haml_tag :li do
+              haml_tag :a, article.heading, :href => path_to(article.abspath)
+            end
+          end
+        end
+      end
+
+      def article_years
+        articles = Page.find_articles
+        last, first = articles[0].date.year, articles[-1].date.year
+        (first..last).to_a.reverse
+      end
+
+      def archive_by_year
+        article_years.each do |year|
+          haml_tag :li do
+            haml_tag :a, :id => "#{year}"
+            haml_tag :h2, year
+            haml_tag :ol do
+              articles = Page.find_articles.select { |a| a.date.year == year }
+              list_articles(articles)
+            end
+          end
+        end
+      end
     end
   end
 
